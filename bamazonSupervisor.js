922c8e2acf00ce338554b0986c5d8a8eb878f99d
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-12-17 22:19:17 -0800

    add function to join products and departments by department name for comparison of sales to overhead costs; I don't remember how many errors I fixed since the last commit so just enjoy this nice working function pls and ty

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -47,3 +47,45 @@ function displaySupervisorOptions() {
         }
     });
 }
+
+// Add function to view overhead costs, product sales, and total profits for each departments
+function viewProductSales() {
+    // Assign variable to select from the relevant columns and combin product_sales by department
+    var query = "SELECT departments.id, departments.department_name, departments.over_head_costs, SUM(products.product_sales)";
+    // Add to query variable to join 'products' and 'departments' tables by department_name
+    query += "FROM products RIGHT JOIN departments ON products.department_name = departments.department_name ";
+    // Add to query variable to group by the 'departments' id and the 'products' department_name
+    query += "GROUP BY departments.id, products.department_name";
+    // Connect to the database using the variable 'query'
+    connection.query(query, function (err, res) {
+        // Assign a variable to create a new CLI-Table
+        var table = new Table({
+            head: ['Department ID', 'Department Name', 'Overhead Costs', 'Product Sales', 'Total Profit'],
+            colWidths: [20, 30, 20, 20, 20]
+        });
+        // Check for errors
+        if (err) throw err;
+        // If no errors...
+        // Iterate through the items in the joined tables
+        res.forEach(function (row) {
+            // Default value of product_sales when column is created in MySQL Workbench is 'null'
+            // So if there haven't been any sales yet, change null to 0 to display correctly in CLI-Table
+            if (row['SUM(products.product_sales)'] === null) {
+                row['SUM(products.product_sales)'] = 0;
+            }
+            // Assign variable to calculate the total profit - sum of product_sales in each row of 'products' table minus the overhead costs in each row
+            var totalProfit = (row['SUM(products.product_sales)'] - row.over_head_costs);
+            // Assign variable to display total to 2 decimal places even if the values at those decimal places are 0
+            var totalDisplay = totalProfit.toFixed(2);
+            // Log for testing
+            // console.log(row.id, row.department_name, row.over_head_costs, row['SUM(products.product_sales)'], totalDisplay);
+            // Push the item and department information, as well as the totalProfit calculation, to the new CLI-Table
+            table.push([row.id, row.department_name, row.over_head_costs, row['SUM(products.product_sales)'], totalDisplay]);
+
+        });
+        // Log the table of product sales by department to the 'supervisor'
+        console.log(table.toString());
+        // Invoke the displaySupervisorOptions function to present user with management action options
+        displaySupervisorOptions();
+    });
+}
